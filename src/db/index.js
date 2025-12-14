df42af75053b06563c13c3aeb1176584e41385b1
codezenashish <ixtashish@gmail.com>
2025-12-14 17:16:29 +0530

    feat: update project structure and add MongoDB connection
    - Changed project type to module in package.json
    - Added development script using nodemon for automatic restarts
    - Introduced dotenv for environment variable management
    - Created a new database connection module in src/db/index.js
    - Added DB_NAME constant for database name configuration
    - Updated index.js to establish MongoDB connection using mongoose
    - Included necessary dependencies: dotenv, express, and mongoose

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../contants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MOGODB_URL}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDb connected !! DB Host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connetction error");
+    process.exit(1);
+  }};
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+// require('dotenv').config()
+
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "./contants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+connectDB();
+
+/*
+
+(async () => {
+  try {
+   await mongoose.connect(`${process.env.MOGODB_URL}/${DB_NAME}`);
+  } catch (error) {
+    console.log("error", error);
+    throw error;
+  }
+})();
+
+*/
