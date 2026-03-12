b9a9d3babce1009628f932f80e8920d0d75975d7
codezenashish <ixtashish@gmail.com>
2026-03-12 17:11:41 +0530

    fix: correct avatar file handling and error messages in user registration

--- 03-restart/src/controllers/user.controller.js
+++ 03-restart/src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
-import { uploadFileToCloudinary } from "../service/Cloudinary.js";
+import uploadFileToCloudinary from "../service/Cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, username, password } = req.body;
@@ -19,18 +19,30 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(409, "user with name all ready exist");
   }
 
-  const avtarLocalPath = req.files?.avatar[0]?.path;
-  const backCoverLocalPath = req.files?.coverImage[0]?.path;
+  console.log("req.files:", req.files);
+  const avtarLocalPath = req.files?.avatar?.[0]?.path;
+
+  let backCoverLocalPath;
+  if (
+    req.files?.coverImage &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    backCoverLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avtarLocalPath) {
-    throw new ApiError(400, "avatar file is requird");
+    throw new ApiError(400, "avatar file is required");
   }
 
   const avtar = await uploadFileToCloudinary(avtarLocalPath);
-  const coverImage = await uploadFileToCloudinary(backCoverLocalPath);
+  let coverImage;
+  if (backCoverLocalPath) {
+    coverImage = await uploadFileToCloudinary(backCoverLocalPath);
+  }
 
   if (!avtar) {
-    throw new ApiError(400, "avatar file is requird");
+    throw new ApiError(400, "avatar file is required");
   }
 
   const user = await User.create({
@@ -45,7 +57,7 @@ const registerUser = asyncHandler(async (req, res) => {
   const createdUser = await User.findById(user._id).select(
     "-password -refreshToken"
   );
-
+  
   if (!createdUser) {
     throw new ApiError(500, "somthing want wrong");
   }
