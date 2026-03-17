3f0e9c43e9a59df4108de2cc9eb3eaf539911967
codezenashish <ixtashish@gmail.com>
2026-03-17 12:13:33 +0530

    fix: update loginUser validation to require either username or email

--- 03-restart/src/controllers/user.controller.js
+++ 03-restart/src/controllers/user.controller.js
@@ -83,7 +83,7 @@ const registerUser = asyncHandler(async (req, res) => {
 // ----
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password, username } = req.body;
-  if (!username || !email) {
+  if (!username && !email) {
     throw new ApiError(400, "username or email is required");
   }
 
