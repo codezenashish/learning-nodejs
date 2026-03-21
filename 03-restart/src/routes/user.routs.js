d1728f20047b6fd1a5c98ce7bb400061230ebc73
codezenashish <ixtashish@gmail.com>
2026-03-21 11:52:50 +0530

    feat: Add routes for user profile management, password changes, and watch history.

--- 03-restart/src/routes/user.routs.js
+++ 03-restart/src/routes/user.routs.js
@@ -4,6 +4,13 @@ import {
   loginUser,
   logoutUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateAvatarDetails,
+  updateCoverImageDetails,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { fileUploadMiddleware } from "../middlewares/multer.moddleware.js";
 import { verifyJWT } from "../controllers/auth.controller.js";
@@ -26,5 +33,20 @@ router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, fileUploadMiddleware.single("avatar"), updateAvatarDetails);
+router
+  .route("/cover-image")
+  .patch(
+    verifyJWT,
+    fileUploadMiddleware.single("coverImage"),
+    updateCoverImageDetails
+  );
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
